ea89ba798937d09427be35e66e321e845dc0a035
Kane Toomer <kanetoomer@yahoo.com>
2025-02-14 09:25:39 -0800

    frontend updated as well

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -77,7 +77,7 @@ export default function Dashboard({ url }) {
               </TransitionChild>
               {/* Sidebar component, swap this element with another sidebar if you like */}
               <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-                <div className="flex h-16 shrink-0 items-center">
+                <div className="flex h-16 shrink-0 items-center font-bold text-indigo-800">
                   <h1>Community Health Dashboard</h1>
                 </div>
                 <nav className="flex flex-1 flex-col">
@@ -127,7 +127,7 @@ export default function Dashboard({ url }) {
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-            <div className="flex h-16 shrink-0 items-center">
+            <div className="flex h-16 shrink-0 items-center font-bold text-indigo-800">
               <h1>Community Health Dashboard</h1>
             </div>
             <nav className="flex flex-1 flex-col">
